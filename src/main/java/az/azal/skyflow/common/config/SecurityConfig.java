5b80e01444577598f54a7bb247628c192930250c
Huseyn Zulfugarov <130462640+huseynzulfuqarov@users.noreply.github.com>
2026-05-22 16:16:44 +0400

    chore: add core deps (JWT, Redis, MapStruct, Flyway), disable Security, configure logging, add app.yaml, set up Docker Compose with PostgreSQL and Redis

---
+++ src/main/java/az/azal/skyflow/common/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package az.azal.skyflow.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() //muveqqeti
+                );
+        return http.build();
+    }
+}
